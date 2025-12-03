15887a4a8bb3c3f102463382e56f84665893a00b
JK1909130 <khuong19092005@gmail.com>
2025-12-03 23:41:09 +0800

    Update code.cpp

--- code.cpp
+++ code.cpp
@@ -1,255 +1,421 @@
-
-#include <iostream>
+#include<iostream>
+#include<vector>
+#include <cstddef>
 #include <ctime>
 using namespace std;
-class Food
+
+
+class Item 
+{
+    public:
+    int id;
+    string name;
+    string description;
+    float weight; //in grams
+};
+class Healing : public Item
+{
+    public:
+    int heal_value;
+    Healing(string name, int val): heal_value(val)
+    {
+        this->name = name;
+    }
+};
+
+class Weapon : public Item
+{
+    public:
+    float damage;
+};
+
+class Light_rounds : public Item
+{
+    public:
+    int damage_boost;
+    Light_rounds* prev;
+    Light_rounds* next;
+    Light_rounds()
+    {
+        damage_boost = 0;
+        this->prev = nullptr;
+        this->next = nullptr;
+    }
+    Light_rounds(int boost): damage_boost(boost), prev(nullptr), next(nullptr) {}
+    ~Light_rounds()
+    {
+        
+    }
+};
+class Heavy_rounds : public Item //it's gonna be a node in a stack data structure
+{
+    public:
+    int damage_boost;
+    Heavy_rounds* prev;
+    Heavy_rounds* next;
+    Heavy_rounds()
+    {
+        damage_boost = 0;
+        this->prev = nullptr;
+        this->next = nullptr;
+    }
+    Heavy_rounds(int boost): damage_boost(boost), prev(nullptr), next(nullptr) {}
+    ~Heavy_rounds()
+    {
+        
+    }
+};
+class Shell : public Item
+{
+    public:
+    int damage_boost;
+    Shell* prev;
+    Shell* next;
+    Shell()
+    {
+        damage_boost = 0;
+        this->prev = nullptr;
+        this->next = nullptr;
+    }
+    Shell(int boost): damage_boost(boost), prev(nullptr), next(nullptr) {}
+    ~Shell()
+    {
+        
+    }
+};
+class Magazine : public Item
+{
+    public:
+    int max;
+};
+
+////////////////////////////////////////////////
+
+// template <typename T>
+// class MagStack : public Magazine
+// {
+// public:
+//     int current_count;
+//     T* cursor; // top of the stack
+
+//     MagStack(int max_size = 5)
+//     {
+//         cursor = nullptr;
+//         current_count = 0;
+//         max = max_size;
+//     }
+
+//     void load(T* round)
+//     {
+//         if (current_count >= max)
+//         {
+//             std::cout << "mag full\n";
+//             return;
+//         }
+
+//         round->prev = nullptr;
+//         round->next = cursor;
+
+//         if (cursor)
+//             cursor->prev = round;
+
+//         cursor = round;
+//         current_count++;
+//     }
+
+//     T* unload() // remove top round
+//     {
+//         if (!cursor) return nullptr;
+
+//         T* top = cursor;
+//         cursor = cursor->next;
+
+//         if (cursor)
+//             cursor->prev = nullptr;
+
+//         top->next = nullptr;
+//         top->prev = nullptr;
+
+//         current_count--;
+//         return top;
+//     }
+
+//     ~MagStack()
+//     {
+//         T* tmp;
+//         while (cursor)
+//         {
+//             tmp = cursor->next;
+//             delete cursor;
+//             cursor = tmp;
+//         }
+//     }
+// };
+
+
+
+////////////////////////////////////////////////
+//Learn this
+class Rifle_mag : public Magazine //the stack
+{
+   int current_rounds;
+   Heavy_rounds* cursor; //to point at the wanted round 
+   Rifle_mag()
+   {
+    cursor = nullptr;
+    max = 5;
+    current_rounds = 0;
+   } 
+   void load(Heavy_rounds* round)
+   {
+    if(current_rounds==0)
+    {
+        round->next = nullptr;
+        cursor = round;
+        current_rounds++;
+    }else if(current_rounds>0 && current_rounds<max)
+    {
+        round->prev = nullptr;
+        round->next = cursor;
+        cursor->prev = round;
+        cursor = round;
+        current_rounds++;
+    }else {cout<<"mag full\n";}
+   }
+   ~Rifle_mag()
+    {
+        // Clean up all nodes in the stack
+        Heavy_rounds* tmp;
+        while (cursor)
+        {
+            tmp = cursor->next;
+            delete cursor;
+            cursor = tmp;
+        }
+    }
+};
+
+
+class Shotgun_mag : public Magazine
 {   
     public:
-    int chance = 0;
-    int distance = 0; 
+   int current_shells;
+   Shell* cursor; //to point at the wanted shell 
+   Shotgun_mag()
+   {
+    cursor = nullptr;
+    max = 5;
+    current_shells = 0;
+   } 
+   void load(Shell* shell)
+   {
+    if(current_shells==0)
+    {
+        shell->next = nullptr;
+        cursor = shell;
+        current_shells++;
+    }else if(current_shells>0 && current_shells<max)
+    {
+        shell->prev = nullptr;
+        shell->next = cursor;
+        cursor->prev = shell;
+        cursor = shell;
+        current_shells++;
+    }else {cout<<"mag full\n";}
+   }
+   ~Shotgun_mag()
+    {
+        // Clean up all nodes in the stack
+        Shell* tmp;
+        while (cursor)
+        {
+            tmp = cursor->next;
+            delete cursor;
+            cursor = tmp;
+        }
+    }
+    
 };
-class Drink
-{   public:
-    int chance = 0;
-    int distance = 0;
+class Handgun_mag : public Magazine
+{
+   int current_rounds;
+   Light_rounds* cursor; //to point at the wanted round 
+   Handgun_mag()
+   {
+    cursor = nullptr;
+    max = 5;
+    current_rounds = 0;
+   } 
+   void load(Light_rounds* round)
+   {
+    if(current_rounds==0)
+    {
+        round->next = nullptr;
+        cursor = round;
+        current_rounds++;
+    }else if(current_rounds>0 && current_rounds<max)
+    {
+        round->prev = nullptr;
+        round->next = cursor;
+        cursor->prev = round;
+        cursor = round;
+        current_rounds++;
+    }else {cout<<"mag full\n";}
+   }
+   ~Handgun_mag()
+    {
+        // Clean up all nodes in the stack
+        Light_rounds* tmp;
+        while (cursor)
+        {
+            tmp = cursor->next;
+            delete cursor;
+            cursor = tmp;
+        }
+    }
 };
-class Weapon
+class Gun : public Weapon
 {
     public:
-    string name;
-    int durability;
-    float damage;
-    int slot; // 1 for two-handed , 2 for small-dual-wieldable,
-    float weight;
+
 };
-class Firearm : public Weapon
+class Revolver : public Gun
 {
-    float range;
-    int accessory; // 1 silencer, 2 stablizer, 3 enhancer
-    int max_ammo;
-    int current_ammo;
+    public:
+};
+class Rifle : public Gun
+{
+    public:
+};
+class Shotgun : public Gun
+{
+    public:
+};
+class DBarrel_Shotgun : public Gun
+{
+    public:
 };
 class Melee : public Weapon
 {
-
+    int reach;
 };
+class Bag
+{   
+    public:
+    vector<Item*> items;
+    void add_to_bag(Item* thing)
+    {
+        items.push_back(thing);
+    }
+};
+
 class Player
 {
-public:
-double velocity;
-int thirst;
-int hunger;
-double coords;
-Weapon main_hand;
-Weapon off_hand;
-Player()
-{
-    velocity = 0; //human max walking speed here is 5km/h or 5000m/h
-    thirst = 0;
-    hunger = 0;
-    coords = 10000.0;
-}
-Player(char diff) //set player coordinates based on diff,  normally the hand spawns at coords 0 and thee player somewhere further. By reeducing the player's coords thee game geets harder
-{
-    if(diff == '1')
-    {
-        coords = 30000.0;
-    }else if(diff == '2')
-    {
-        coords = 20000.0;
-    }else if(diff == '3')
-    {
-        coords = 10000.0;
-    }else throw "invalid difficulty\n";
-} 
-void setDiff(int diff)
-{
-    this->coords = 30000.0/diff;
-}
+    public:
+    string name;
+    float health;
+    float saturation;
+    float hydration;
+    float illness;
+    float position; //in meters
+    float velocity;
+    Weapon* main_hand;
+    Weapon* off_hand;
+    Weapon* melee;
+    Bag* bag;
+
+    Player()
+    {   name = "zero";
+        health = 100;
+        saturation = 100;
+        hydration = 100;
+        illness = 1;
+        main_hand = nullptr;
+        off_hand = nullptr;
+        melee = nullptr;
+        bag = nullptr;
+        position = 0;
+        velocity = 4.0; //  m/s
+    }
+    Player(string name, Bag* b, Weapon* main , Weapon* off, int position):name(name), bag(b), main_hand(main), off_hand(off), position(position){
+        health = 100;
+        saturation = 100;
+        hydration = 100;
+        illness = 1;
+    }
+    
+};
+class Enemy
+{   public:
+    string name;
+    int health;
+    Weapon* weapon;
 };
 class Hand
 {
-public:
-    double velocity;
-    double coords;
-
-    Hand(): coords(0.0), velocity(0.0){}
-    void refresh(){}
+    public:
+    float position; //also in meters
+    float velocity = 4.0 + 4.0 * ((0.023148148 - 0.01) / 4.0) + 0.01;
 };
+
+
+class Combat
+{
+    Player* p;
+    Enemy* e;
+    Combat()
+    {
+        p = new Player();
+        e = new Enemy();
+    }
+    Combat(Player* p, Enemy* e): p(p), e(e){}
+    ~Combat() 
+    {
+        delete p;
+        delete e;
+    }
+    void playerAttacks(Weapon* w)
+    {
+        if(w != nullptr)
+        {
+            e->health-=w->damage;
+            cout<<p->name<<" dealt "<<w->damage<<" to "<<e->name<<"\n";
+        }else{
+            cout<<"No weapon! "<<p->name<<" can't attack\n";
+        }
+    }
+    void playerAttacks(Revolver* w)
+    {
+        if(w != nullptr)
+        {
+            e->health-=w->damage;
+            cout<<p->name<<" dealt "<<w->damage<<" to "<<e->name<<"\n";
+
+        }else{
+            cout<<"No weapon! "<<p->name<<" can't attack\n";
+        }
+    }
+    void enemyAttacks(Melee* w)
+    {
+        p->health-=w->damage;
+        cout<<e->name<<" dealt "<<w->damage<<" to "<<p->name<<"\n";
+    }
+    void playerHeals(Healing* h)
+    {
+        if(h != nullptr)
+        {p->health+=h->heal_value;}
+        else{cout<<"No healing items\n";}
+    }
+};
+
+
+
+
 int main()
 {
-    srand(time(NULL));
-    Hand h;
-    Player p;
-    ah:
-    int diffi = 1;
-    cout<<"\n\nChoose difficulty:\n 1. Easy  2. Medium  3. Hard\n and press Enter\n";
-    cin>>diffi;
-    switch(diffi)
+    for(int hour=1;hour<=24;hour++)
     {
-        case 1: 
-        {
-            cout<<"- The hand always move slightly faster than you.\n- You have 24hrs until it switches victim.\n";
-            cout<<"The Hand is 30 km from you.\n";
-            p.setDiff(1);
-            break;
-        }
-        case 2: 
-        {
-            cout<<"- The hand always move slightly faster than you.\n- You have 24hrs until it switches victim.\n";
-            cout<<"The Hand is 15 km from you.\n";
-            p.setDiff(2);
-            break;
-        }
-        case 3: 
-        {
-            cout<<"- The hand always move slightly faster than you.\n- You have 24hrs until it switches victim.\n";
-            cout<<"The Hand is 10 km from you.\n";
-            p.setDiff(3);
-            break;
-        }
-        default: {
-                cout<<"invalid difficulty\n";
-                goto ah;
-                 }
-    }
-    for(int i=0;i<24;i++)
-    {
-        cout<<"Hr #"<<i+1<<": \n";
-        cout<<"Saturation: "<<100-p.hunger<<"\n";
-        cout<<"Hydration: "<<100-p.thirst<<"\n";
-        cout<<"The Hand's distance from you: "<<p.coords-h.coords<<"m(s)"<<"\n\n";
-        Food f;
-        Drink d;
-        f.chance = (rand()%3)+1;
-        if(f.chance == 1)
-        {   
-            int posNeg = rand()%2;
-            if(posNeg == 0)
-            {
-                f.distance = (rand()%300)+1;
-            }else {f.distance = ((rand()%300)+1)*-1;}
-        }
-        d.chance = (rand()%3)+1;
-        if(d.chance == 1)
-        {   
-            int posNeg = rand()%2;
-            if(posNeg == 0)
-            {
-                d.distance = (rand()%300)+1;
-            }else {d.distance = ((rand()%300)+1)*-1;}
-        }//////////////////////////////////////////////////////
-        if(p.thirst>=100||p.hunger>=100||h.coords>=p.coords)
-        {
-            cout<<"GAME OVER\n\n";
-            
-        }
-        if(i==23)
-        {
-            cout<<"YOU WIN\n\n";
-            
-        }/////////////////////////////////////////////////////
-        if(f.chance==1)
-        {
-            if(f.distance>0)
-            {
-                cout<<"food location: "<<f.distance<<"m away from the Hand\n";
-            }else if(f.distance<0)
-            {
-                cout<<"food location: "<<abs(f.distance)<<"m towards the Hand\n";
-            }
-        }else{cout<<"no food available nearby\n";}
 
-        if(d.chance==1)
-        {
-            if(d.distance>0)
-            {
-                cout<<"drinks location: "<<d.distance<<"m away from the Hand\n";
-            }else if(d.distance<0)
-            {
-                cout<<"drinks location: "<<abs(d.distance)<<"m towards the Hand\n";
-            }
-        }else{cout<<"no drinks available nearby\n";}
-        //add spawn chance and show distance and direction to player  DONE
-        cout<<"\n\n";
-        cout<<"-------------------------\n";
-        cout<<"| Actions:              |\n";
-        cout<<"| 1. move towards food  |\n";
-        cout<<"| 2. move towards water |\n";
-        cout<<"| 3. move...            |\n";
-        cout<<"| 4. stay still         |\n";
-        cout<<"| 5. Exit               |\n";
-        cout<<"-------------------------\n";
-        cout<<"If there are no sustenance and you try to move towards them, nothing will happen\n";
-        int input;
-        cin>>input;
-        switch(input)
-        {
-            case 1: //towards food
-                if(f.chance==1)
-                {
-                    p.coords+=f.distance;
-                    p.velocity=f.distance/60;
-                    h.coords+=(p.velocity+p.velocity*0.1)*60;
-                    p.hunger-=40;
-                    p.thirst+=20;
-                    if(p.hunger<0){p.hunger=0;}
-                }else{
-                    p.velocity=0.0;
-                    h.coords+=(1+1*0.1)*60;
-                    p.hunger+=20;
-                    p.thirst+=20;
-                }
-                break;
-            case 2:  //towards water
-                if(d.chance==1)
-                {
-                    p.coords+=d.distance;
-                    p.velocity=d.distance/60;
-                    h.coords+=(p.velocity+p.velocity*0.1)*60;
-                    p.thirst-=40;
-                    p.hunger+=20;
-                    if(p.thirst<0){p.thirst=0;}
-                }else{
-                    p.velocity=0.0;
-                    h.coords+=(1+1*0.1)*60;
-                    p.thirst+=10;
-                    p.hunger+=10;
-                }
-                break;
-            case 3: //move in a direction
-                {
-                    cout<<"enter coordinate to move to (m)(negative to move towards the Hand): ";
-                    double movement;
-                    cin>>movement;
-                    if(abs(movement)>0)
-                    {
-                        p.coords+=movement;
-                        p.velocity=movement/60;
-                        h.coords+=(p.velocity+p.velocity*0.1)*60;
-                        p.thirst+=20;
-                        p.hunger+=20;
-                    }else{
-                        p.velocity=0.0;
-                        h.coords+=(1+1*0.1)*60;
-                        p.thirst+=10;
-                        p.hunger+=10;
-                    }
-                }
-                break;
-            case 4:
-                {
-                        p.velocity=0.0;
-                        h.coords+=(1+1*0.1)*60;
-                        p.thirst+=10;
-                        p.hunger+=10;
-                }break;
-            case 5: return 0; break;
-        }
-        cout<<"\n\n\n";
     }
     return 0;
 }
-
-
-
